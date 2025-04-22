7a1c942f9b30a8f48b7b3366317ce061bdfda9f3
NagesoHaile <nageehail12@gmail.com>
2025-04-22 11:01:55 +0300

    feat: auth middleware setup. -> attaching a user id to each request using jwt bearer token.

---
+++ middleware/auth.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException,Request
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
+from app.config.database import get_session
+from app.utils.auth import decode_token
+
+class AuthMiddleware(HTTPBearer):
+    def __init__(self,auto_error:bool = True):
+        super().__init__(auto_error=auto_error)
+    
+    async def __call__(self, request:Request):
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
+
+
+        if not credentials:
+            raise HTTPException(
+                status_code=401,detail="Authorization token missing"
+            )
+        # session: Session = next(get_session)
+        try:
+            user_id =decode_token(credentials.credentials)
+        except Exception:
+            raise HTTPException(
+                status_code=401, 
+                detail="Invalid token or token has expired"
+            )
+        request.state.id = user_id
+        # return await super().__call__(request)
