2472ae4573b29ad51214e339a90f0d3228a73b53
NagesoHaile <nageehail12@gmail.com>
2025-04-22 11:06:11 +0300

    feat: database initialization in main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.config.database import get_session,init_db
+@asynccontextmanager
+async def lifespan(ap:FastAPI):
+    init_db()
+    yield
 
 
-app = FastAPI()
 
-@app.get('')
+app = FastAPI(lifespan=lifespan)
+
+@app.get('/')
 def index():
     return {"Hello World"}
